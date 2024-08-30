dec4464b8a3fe7ae876476ee36164d90d3366bb3
Asirwad <asirwadsali@gmail.com>
2024-08-30 11:19:46 +0530

    Initial commit

---
+++ src/main/java/com/ust/spring_ai_llama_chat/SpringAiLlamaChatApplication.java
@@ -0,0 +1,13 @@
+package com.ust.spring_ai_llama_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiLlamaChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiLlamaChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ust/spring_ai_llama_chat/SpringAiLlamaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.spring_ai_llama_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiLlamaChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
